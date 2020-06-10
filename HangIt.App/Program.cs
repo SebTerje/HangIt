81bc84edf6113e140760947162b63b1a15afb913
sebTerje <sebastianmennen@gmail.com>
2020-06-10 10:33:36 +0200

    Hello World multiple times

--- HangIt.App/Program.cs
+++ HangIt.App/Program.cs
@@ -7,6 +7,10 @@ namespace HangIt.App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
